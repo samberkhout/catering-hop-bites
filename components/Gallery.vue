5c97dd2040d7527feb0c4f37226aa5067585461e
samberkhout <144678304+samberkhout@users.noreply.github.com>
2025-06-25 16:25:35 +0200

    Initialize Nuxt project and remove favicon

---
+++ components/Gallery.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <img
+      v-for="foto in fotos"
+      :key="foto"
+      :src="foto"
+      loading="lazy"
+      class="rounded shadow-lg transform hover:scale-105 transition-transform duration-300"
+      alt="galerijfoto"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ type: string }>();
+
+const { data } = await useFetch<{ fotos: string[] }>(`/api/fotos?type=${props.type}`);
+const fotos = data.value?.fotos || [];
+</script>
